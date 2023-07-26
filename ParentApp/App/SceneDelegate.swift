9b0fc8c0b0010b339681fd8a90bbaa282275837f
DeVIn4I <i@dizzyfly.ru>
2023-07-26 22:48:14 +0300

    Настроить проект

---
+++ ParentApp/App/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  ParentApp
+//
+//  Created by Pavel Razumov on 26.07.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = ParentViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
